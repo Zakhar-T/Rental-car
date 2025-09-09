066553fca5a845ba570db447686bbe3896dc148c
Zakhar-T <zakhar.null@gmail.com>
2025-09-09 23:44:52 +0300

    completed home page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,25 @@
+import clsx from 'clsx';
 import styles from './Header.module.css';
 
 import { Link, NavLink } from 'react-router-dom';
 
+const buildNavLinkClass = ({ isActive }) => {
+  return clsx(styles.navLink, isActive && styles.activeNavLink);
+};
+
 export default function Header() {
   return (
-    <header className={styles.container}>
-      <Link to="/" aria-label="Go to home"></Link>
+    <header className={clsx('container', styles.headerContainer)}>
+      <Link to="/" className={styles.logo} aria-label="Go to home">
+        Rental<span className={styles.logoPart}>Car</span>
+      </Link>
       <nav className={styles.navWrapper}>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/catalog">Catalog</NavLink>
+        <NavLink className={buildNavLinkClass} to="/">
+          Home
+        </NavLink>
+        <NavLink className={buildNavLinkClass} to="/catalog">
+          Catalog
+        </NavLink>
       </nav>
     </header>
   );
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,17 @@
+import clsx from 'clsx';
 import styles from './HomePage.module.css';
+import { Link } from 'react-router-dom';
 
 export default function HomePage() {
   return (
-    <div className={styles.container}>
-      <h1 className={styles.title}>Find your perfect rental car</h1>
-      <p className={styles.subtitle}>Reliable and budget-friendly rentals for any journey</p>
-    </div>
+    <main className={styles.hero}>
+      <div className={clsx('container', styles.container)}>
+        <h1 className={styles.title}>Find your perfect rental car</h1>
+        <p className={styles.subtitle}>Reliable and budget-friendly rentals for any journey</p>
+        <Link to="/catalog" className={styles.catalogLink}>
+          View Catalog
+        </Link>
+      </div>
+    </main>
   );
 }
